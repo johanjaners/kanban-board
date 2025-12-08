5af0f1129b8bfd4103815ede1031c30bd916871f
Johan Janérs <johanjaners@gmail.com>
2025-12-08 20:35:13 +0100

    Refactor Swagger configuration to always enable in production

--- backend/KanbanBoard.Api/Program.cs
+++ backend/KanbanBoard.Api/Program.cs
@@ -15,11 +15,14 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
