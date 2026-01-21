4e0582c75609d533cf49c044fc25109778c72ddf
Johan Janérs <johanjaners@gmail.com>
2026-01-21 12:32:25 +0100

    feat: add ToastContainer in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,24 @@ import { Home } from "./pages/Home";
 import { TaskList } from "./pages/TaskList";
 import { About } from "./pages/About";
 import { SignedIn, SignedOut, SignIn } from "@clerk/clerk-react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
     return (
         <div className="min-h-screen bg-gray-100 min-w-80">
+            <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
             <main>
                 <SignedOut>
                     <div className="flex justify-center items-center h-[80vh]">
