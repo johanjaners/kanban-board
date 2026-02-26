0176ba47741de1a82240bb914c11e67861ded10b
Johan Janérs <johanjaners@gmail.com>
2026-02-26 09:42:58 +0100

    feat: implement initial test for GetById method in TaskServiceTests

--- backend/KanbanBoard.Api.Tests/TaskServiceTests.cs
+++ backend/KanbanBoard.Api.Tests/TaskServiceTests.cs
@@ -2,9 +2,24 @@
 
 public class TaskServiceTests
 {
-    [Fact]
-    public void Test1()
-    {
+    private readonly Mock<ITaskRepository> _repositoryMock;
+    private readonly TaskService _service;
 
+    public TaskServiceTests()
+    {
+        _repositoryMock = new Mock<ITaskRepository>();
+        _service = new TaskService(_repositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task GetById_ReturnsNull_WhenTaskDoesNotExist()
+    {
+        //Arrange
+        _repositoryMock
+            .Setup(r => r.GetByIdAsync(1, "user1"))
+            .ReturnAsync((TaskItem?)null);
+
+        //Act
+        var res
     }
 }
